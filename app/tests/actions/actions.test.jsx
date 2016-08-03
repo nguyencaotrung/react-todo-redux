c292034576d148f5e270822b04018702e001e75b
Nguyen Cao Trung <trung.nguyen2@trginternational.com.vn>
2016-08-03 15:59:34 +0700

    update todos on firebase

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -2,6 +2,7 @@ var expect = require('expect');
 var actions = require('actions');
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import firebase, {firebaseRef} from 'app/firebase';
 var createMockStore = configureMockStore([thunk]);
 describe('Actions', () => {
   it('should generate search text action', () => {
@@ -71,13 +72,50 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
-  it('should generate toggle todo action', () => {
+  it('should generate UPDATE_TODO action action', () => {
     var action = {
-      type: 'TOGGLE_TODO',
-      id: '123'
+      type: 'UPDATE_TODO',
+      id: '123',
+      updates: {completed: false}
     };
-    var res = actions.toggleTodo(action.id);
+    var res = actions.updateTodo(action.id, action.updates);
 
     expect(res).toEqual(action);
+  });
+  describe('Tests with firebase todos', () => {
+    var testTodoRef;
+
+    beforeEach((done) => {
+      testTodoRef = firebaseRef.child('todos').push();
+
+      testTodoRef.set({
+        text: 'Something todo',
+        completed :false,
+        completedAt: 123456
+      }).then(() => done());
+    });
+    afterEach((done) => {
+      testTodoRef.remove().then(()=>done());
+    });
+    it('should toggle todo and dispatch UPDATE_TODO action ', (done) => {
+      const store = createMockStore({});
+      const action = actions.startToggleTodo(testTodoRef.key,true);
+
+      store.dispatch(action).then(()=>{
+        const mockActions = store.getActions();
+
+        expect(mockActions[0]).toInclude({
+          type: 'UPDATE_TODO',
+          id: testTodoRef.key,
+        });
+
+        expect(mockActions[0].updates).toInclude({
+          completed: true
+        });
+        expect(mockActions[0].updates.completedAt).toExist();
+        done();
+
+      },done);
+    });
   });
 });
