e5589458ab1c720edef3ed6bd2470bafbbff469b
Nguyen Cao Trung <trung.nguyen2@trginternational.com.vn>
2016-08-10 09:17:15 +0700

    Add actions for firebase login logout

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,17 +1,24 @@
-var React = require('react');
-var uuid = require('node-uuid');
-var moment = require('moment');
-
+import * as Redux from 'react-redux';
+import React from 'react';
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
+import * as actions from 'actions';
 
+export var TodoApp = React.createClass({
+  onLogout(e) {
+    var {dispatch} = this.props;
+    e.preventDefault();
 
-var TodoApp = React.createClass({
-  render: function () {
+    dispatch(actions.startLogout());
+  },
+  render() {
     
     return (
       <div>
+        <div className="page-actions">
+          <a href="#" onClick={this.onLogout}>Logout</a>
+        </div>
         <h1 className="page-title">Todo App</h1>
 
         <div className="row">
@@ -28,4 +35,4 @@ var TodoApp = React.createClass({
   }
 });
 
-module.exports = TodoApp;
+export default Redux.connect()(TodoApp);
